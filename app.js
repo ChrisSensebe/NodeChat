15b6d6c60911302f1a861037e1e1739a41be29f9
ChrisSensebe <christophe.sensebe@gmail.com>
2015-10-22 09:45:54 +0200

    ajout index.html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app  = require('express')(),
 	http = require('http').Server(app);
 
 app.get('/', function(req, res){
-	res.send('<h2>Node chat</h2>');
+	res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(8080, function(){
