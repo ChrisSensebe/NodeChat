67bf039e50c1ace0d20ea1ff072491631d42139a
ChrisSensebe <christophe.sensebe@gmail.com>
2015-10-22 10:05:07 +0200

    ajout socket.io

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Dependences
 var app  = require('express')(),
-	http = require('http').Server(app);
+	http = require('http').Server(app),
+	io   = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('nouvelle connection');
 });
 
 http.listen(8080, function(){
