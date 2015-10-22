c8824cb76c78f7dcbdfc11d9acda663cece23551
ChrisSensebe <christophe.sensebe@gmail.com>
2015-10-22 11:09:19 +0200

    ajout echange message client / serveur

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('chat message', function(message){
-		console.log('message: ' + message);
+		console.log(message);
+		io.emit('chat message', message);
 	});
 });
 
