cb9dd491bb72f2a4bbcdd46f28366d2cd3d0b5e7
ChrisSensebe <christophe.sensebe@gmail.com>
2015-10-23 21:21:39 +0200

    adding style.css

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Dependences
-var app  = require('express')(),
-	http = require('http').Server(app),
-	io   = require('socket.io')(http);
+var express = require('express'),
+	app     = express(),
+	http    = require('http').Server(app),
+	io      = require('socket.io')(http);
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
