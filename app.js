03a3a0508505a2f50ade07b033035d0171b0e3a7
ChrisSensebe <christophe.sensebe@gmail.com>
2015-10-25 19:02:37 +0100

    ajout commentaires

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
-// Dependences
+/*
+/	dependances
+*/
 var express = require('express'),
-	app     = express(),
-	http    = require('http').Server(app),
-	io      = require('socket.io')(http);
-
+	http    = require('http'),
+	socket  = require('socket.io');
+/*
+/	chargement de l'application du serveur et de socket.io
+*/
+var app    = express()
+var server = http.Server(app);
+var io     = socket(http);
+/*
+/	sert les fichiers statiques (client.js et style.css)
+/	depuis le repertoire public/
+*/
 app.use(express.static('public'));
-
+/*
+/	sert index.html lors d'une requete get à la racine du serveur
+*/
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
-
+/*
+/	creation d'un socket lors d'une connection
+*/
 io.on('connection', function(socket){
 	console.log('nouvelle connection');
-
+	// gestion de l'evenement deconnection
 	socket.on('disconnect', function(socket){
 		console.log('utilisateur deconnecté');
 	});
-
+	// emission du pseudo a tout les clients a l'evenement 'new client'
 	socket.on('new client', function(pseudo){
 		console.log('Nouvel utilisateur: ' + pseudo);
 		io.emit('new client', pseudo);
 	});
-
+	// emission du message lors de l'evenement 'chat message'
 	socket.on('chat message', function(message){
 		console.log(message.pseudo + ': ' + message.message);
 		io.emit('chat message', message);
 	});
 });
-
-http.listen(8080, function(){
+/*
+/	demarre le serveur sur le port 8080
+*/
+server.listen(8080, function(){
 	console.log('app listening on port 8080');
 });
--- public/client.js
+++ public/client.js
@@ -1,6 +1,17 @@
+/*
+/	se connecte au serveur grace a socket.io
+*/
 var socket = io();
+/*
+/	variable servant a stocker le pseudo
+*/
 var pseudo;
-
+/*
+/	A la soumission du pseudo:
+/	le stocke dans pseudo
+/	le transmet au serveur
+/	cache le formulaire de soumission du pseudo et affiche celui des messages
+*/
 $('#formPseudo').submit(function(){
 	pseudo = $('#pseudo').val();
 	socket.emit('new client', pseudo);
@@ -8,17 +19,24 @@ $('#formPseudo').submit(function(){
 	$('#formPseudo').hide();
 	return false;
 });
-
+/*
+/	transmet le message et son auteur au serveur
+/	vide la zone de saisie
+*/
 $('#formMessage').submit(function(){
 	socket.emit('chat message', {pseudo : pseudo, message : $('#message').val()});
 	$('#message').val('');
 	return false;
 });
-
+/*
+/	affiche un message quand un nouveau client se connecte
+*/
 socket.on('new client', function(pseudo){
 	$('#messages').append($('<li>').text('Serveur : ' + pseudo + ' viens de se connecter'));
 });
-
+/*
+/	affiche les messages recus depuis le serveur
+*/
 socket.on('chat message', function(message){
 	$('#messages').append($('<li>').text(message.pseudo + ' : ' + message.message))
 });
